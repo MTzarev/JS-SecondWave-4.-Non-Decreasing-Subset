40cd3d149e3b6f5357566f3f719ca1487e02c016
Miroslav Tsarev <mtzarev@gmail.com>
2024-08-29 20:59:09 +0300

    Update 4. Non-Decreasing Subset.js

--- 4. Non-Decreasing Subset.js
+++ 4. Non-Decreasing Subset.js
@@ -1,7 +1,6 @@
 function nonDecreasingSubset (arr){
 let newArr = [];
 for (let i=0; i<arr.length; i++){
-    
     if(Number(arr[i])<Number(arr[i-1])||arr[i]<arr[0]){
         continue;
     }else{
